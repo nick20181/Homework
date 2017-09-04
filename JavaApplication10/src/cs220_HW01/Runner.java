2d27327e5b4e46900baf5760e4ae90e70686aa8b
nick201 <nick201@10.153.5.29>
2017-09-04 11:30:19 -0500

    Completed LRU not perfect however

---
+++ JavaApplication10/src/cs220_HW01/Runner.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs220_HW01;
+
+/**
+ *
+ * @author Nicholas Bohm
+ */
+public class Runner {
+    
+    public Runner(){
+        System.out.println("Runner Start ...");
+        SocketHandler blackburn = new SocketHandler("10.100.1.239", 255);
+        System.out.println(blackburn.getConnectionStat());
+    }
+    
+    
+    public static void main(String[] args) {
+        new Runner();
+    }
+}
