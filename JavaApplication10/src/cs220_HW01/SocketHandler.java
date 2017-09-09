6f6e9782166b723edd2c19b8afc057c515cbaed0
nick201 <nick201@10.153.5.29>
2017-09-09 12:51:03 -0500

    Completed LRU not perfect however

--- JavaApplication10/src/cs220_HW01/SocketHandler.java
+++ JavaApplication10/src/cs220_HW01/SocketHandler.java
@@ -15,7 +15,7 @@ public class SocketHandler {
     private DataInputStream incoming;
     private PrintStream outGoing;
     private BufferedReader bufferInput;
-
+    //Creates a socket
     public SocketHandler(String address, int port) {
         try {
             this.currentSocket = new Socket(address, port);
@@ -29,11 +29,11 @@ public class SocketHandler {
            // System.out.println("SH IOE");
         }
     }
-
+    //Sends a command to the socket
     public void sendCommand(String command) {
         this.outGoing.println(command);
     }
-
+    //receives and saves a file
     public void reciveFile(int fileSize, String nameOfFile) {
         try {
             byte[] buffer = new byte[fileSize];
@@ -42,12 +42,10 @@ public class SocketHandler {
             dir.mkdir();
             new FileOutputStream(dir + "\\" + nameOfFile);
         } catch (IOException ex) {
-            ex.printStackTrace();
-            System.out.println("RF IOE");
         }
 
     }
-
+    //receives the string msg from the server
     public String revceiveString() {
         
         try {
@@ -56,7 +54,7 @@ public class SocketHandler {
             return "error in RevicedString";
         }
     }
-
+    //closes the server
     public void close() {
         try {
             this.currentSocket.close();
